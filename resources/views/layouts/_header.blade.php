2b9a46927cb39ab0e65d2bc5e238c9c463476081
liangminghaoAngus <liangminghaoAngus@163.com>
2018-02-03 11:58:14 +0000

    Notify author when topic replied

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,7 +13,7 @@
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
                 RCS BBS
-                <!--仁爱菁英大学生论坛  -->
+                <!-- 仁爱菁英大学生论坛  -->
             </a>
         </div>
 
@@ -30,16 +30,24 @@
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
-                @guest
-                    <li><a href="{{ route('login') }}">登录</a></li>
-                    <li><a href="{{ route('register') }}">注册</a></li>
-                @else
-                    <li>
-                        <a href="{{ route('topics.create') }}">
-                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                        </a>
-                    </li>
-                    <li class="dropdown">
+               @guest
+                   <li><a href="{{ route('login') }}">登录</a></li>
+                   <li><a href="{{ route('register') }}">注册</a></li>
+               @else
+                   <li>
+                       <a href="{{ route('topics.create') }}">
+                           <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                       </a>
+                   </li>
+                   {{-- 消息通知标记 --}}
+                   <li>
+                       <a href="{{ route('notifications.index') }}" class="notifications-badge" style="margin-top: -2px;">
+                           <span class="badge badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'fade' }} " title="消息提醒">
+                               {{ Auth::user()->notification_count }}
+                           </span>
+                       </a>
+                   </li>
+                   <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                                 <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
